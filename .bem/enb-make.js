88ef5ed5cf4bea334e5064924e45f651a9e1725e
Dmitry Sorin <dsorin@yandex-team.ru>
2014-03-27 16:05:14 +0400

    ..

--- .bem/enb-make.js
+++ .bem/enb-make.js
@@ -1,26 +1,56 @@
-module.exports = function (config) {
-    config.node('pages/index', function (nodeConfig) {
-        nodeConfig.addTechs([
-            require('enb/techs/bemdecl-from-bemjson')
-            [ require('enb/techs/levels'), { levels: getLevels(config) } ],
-            [ require('enb/techs/file-provider'), { target: '?.bemdecl.js' } ],
-            require('enb/techs/deps-old'),
-            require('enb/techs/files'),
-            require('enb/techs/js'),
-            require('enb/techs/css'),
-            [ require('enb/techs/file-copy'), { sourceTarget: '?.js', destTarget: '_?.js' } ],
-            [ require('enb/techs/file-copy'), { sourceTarget: '?.css', destTarget: '_?.css' } ]
-        ]);
+module.exports = function(config) {
+    config.setLanguages(["ru", "en"]);
+    config.mode("development", function() {
 
-        nodeConfig.addTargets(['_?.js', '_?.css']);
     });
-};
+    config.mode("production", function() {
 
-function getLevels(config) {
-    return [
-      { path: 'bem-core/common.blocks', check: false },
-      { path: 'bem-core/desktop.blocks', check: false },
-      // 'common.blocks',
-      'desktop.blocks'
-    ].map(function(levelPath) { return config.resolvePath(levelPath); });
+    });
+
+    config.node("desktop.bundles/index", function(nodeConfig) {
+        nodeConfig.addTechs([
+            [ require("enb/techs/levels"), { levels: getLevels() } ],
+            [ require("enb/techs/file-provider"), { target: "?.bemjson.js" } ],
+            require("enb/techs/bemdecl-from-bemjson"),
+            require("enb/techs/deps-old"),
+            require("enb/techs/files"),
+            require("enb-bemhtml/techs/bemhtml"),
+            require("enb/techs/html-from-bemjson")
+        ]);
+        nodeConfig.addTargets(["?.html"]);
+
+        function getLevels() {
+            return [
+                {"path":"bem-core/common.blocks","check":true},
+                {"path":"bem-core/desktop.blocks","check":true}
+            ].map(function(l) { return config.resolvePath(l); });
+        }
+    });
 }
+
+// module.exports = function (config) {
+//     config.node('pages/index', function (nodeConfig) {
+//         nodeConfig.addTechs([
+//             require('enb/techs/bemdecl-from-bemjson')
+//             [ require('enb/techs/levels'), { levels: getLevels(config) } ],
+//             [ require('enb/techs/file-provider'), { target: '?.bemdecl.js' } ],
+//             require('enb/techs/deps-old'),
+//             require('enb/techs/files'),
+//             require('enb/techs/js'),
+//             require('enb/techs/css'),
+//             [ require('enb/techs/file-copy'), { sourceTarget: '?.js', destTarget: '_?.js' } ],
+//             [ require('enb/techs/file-copy'), { sourceTarget: '?.css', destTarget: '_?.css' } ]
+//         ]);
+
+//         nodeConfig.addTargets(['_?.js', '_?.css']);
+//     });
+// };
+
+// function getLevels(config) {
+//     return [
+//       { path: 'bem-core/common.blocks', check: false },
+//       { path: 'bem-core/desktop.blocks', check: false },
+//       // 'common.blocks',
+//       'desktop.blocks'
+//     ].map(function(levelPath) { return config.resolvePath(levelPath); });
+// }
--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,5 +1,5 @@
 ({
-    block: 'b-page',
+    block: 'page',
     title: 'BEM Pokormikota',
     favicon: '/favicon.ico',
     head: [
@@ -11,77 +11,6 @@
         { elem: 'meta', attrs: { name: 'keywords', content: '' }}
     ],
     content: [
-        {
-            block: 'body-wrapper',
-            content: [
-                {
-                    block: 'header',
-                    content: [
-                        { elem: 'logo', src: 'http://pokormikota.ru/pics/logo.jpg' },
-                        { elem: 'cart' },
-                        { elem: 'worktime' },
-                        { elem: 'adv_korma' }
-                    ]
-                },
-                {
-                    block: 'menu',
-                    content: [
-                        { elem: 'Магазин' },
-                        { elem: 'Условия доставки' },
-                        { elem: 'Бонусы' },
-                        { elem: 'Контакты' },
-                        {
-                            elem: 'Вход',
-                            mods: { pos: 'right' }
-                        }
-                    ]
-                },
-                {
-                    block: 'submenu',
-                    content: [
-                        { elem: 'Корма' },
-                        { elem: 'Наполнители' },
-                        { elem: 'Переноски' },
-                        { elem: 'Туалеты' },
-                        { elem: 'Когтеточки' },
-                        { elem: 'Игрушки' },
-                        { elem: 'Груминг' },
-                        { elem: 'Аксессуары' },
-                        { elem: 'Сувениры' },
-                        { elem: 'Уценка' }
-                    ]
-                },
-                {
-                    block: 'content',
-                    content: [
-                        {
-                            block: 'header',
-                            elem: 'Интернет-магазин для кошек и их хозяев'
-                        },
-                        {
-                            block: 'text',
-                            elem: 'pokormikota.ru - это специализированный интернет-магазин для кошек и их хозяев. У нас вы сможете заказать корма и множество других товаров необходимых для содержания и ухода за вашим любимым питомцем.'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            block: 'footer',
-            content: [
-                {
-                    block: 'menu',
-                    content: [
-                        { elem: 'Магазин' },
-                        { elem: 'Условия доставки' },
-                        { elem: 'Бонусы' },
-                        { elem: 'Контакты' }
-                    ]
-                },
-                { elem: 'adv_rc' },
-                { elem: 'adv_rc_diary' },
-                { elem: 'copyright' }
-            ]
-        }
+
     ]
 })
