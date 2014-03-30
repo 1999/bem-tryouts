19ae350b80babb73c84852451894882ab4d33e95
Dmitry Sorin <info@staypositive.ru>
2014-03-30 20:18:49 +0400

    Add logo elem impl

--- desktop.blocks/logo/logo.bh.js
+++ desktop.blocks/logo/logo.bh.js
@@ -1,7 +1,18 @@
 module.exports = function (bh) {
-    bh.match('logo', function (ctx) {
-        ctx
-            .tag('a')
-            .attr('href', '/');
+    bh.match({
+        logo: function (ctx) {
+            ctx
+                .tag('a')
+                .attr('href', '/')
+                .content({
+                    elem: 'image',
+                    image: ctx.param('image')
+                }, true);
+        },
+        logo__image: function (ctx) {
+            ctx
+                .tag('img')
+                .attr('src', ctx.param('image'));
+        }
     });
 };
