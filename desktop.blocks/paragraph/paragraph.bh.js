b960c424b972330fc0904faeeb23355760622ed3
Dmitry Sorin <info@staypositive.ru>
2014-03-30 21:43:00 +0400

    ..

--- desktop.blocks/paragraph/paragraph.bh.js
+++ desktop.blocks/paragraph/paragraph.bh.js
@@ -16,31 +16,4 @@ module.exports = function (bh) {
                 .content(ctx.param('text'));
         }
     });
-
-
-    //   {
-    //     'menu': function(ctx) {
-    //         ctx.tag('ul');
-    //     },
-    //     'menu__item': function(ctx) {
-    //         ctx
-    //             .tag('li');
-
-    //         var text = ctx.param('text');
-    //         var url = ctx.param('url');
-
-    //         if (text) {
-    //             if (url) {
-    //                 ctx.content({
-    //                     block: 'link',
-    //                     url: url,
-    //                     text: text,
-    //                     cls: ctx.param('cls') || []
-    //                 }, true);
-    //             } else {
-    //                 ctx.content(text);
-    //             }
-    //         }
-    //     }
-    // });
 };
