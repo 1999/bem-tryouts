082b5b0a7156fa577d495ecbbb643ed5d30dfc4a
Dmitry Sorin <info@staypositive.ru>
2014-06-24 08:40:15 +0400

    Update enb make

--- .bem/enb-make.js
+++ .bem/enb-make.js
@@ -9,17 +9,22 @@ module.exports = function(config) {
 
     config.node("desktop.bundles/index", function(nodeConfig) {
         nodeConfig.addTechs([
-            [ require("enb/techs/levels"), { levels: getLevels() } ],
-            [ require("enb/techs/file-provider"), { target: "?.bemjson.js" } ],
+            [ require('enb-bh/techs/bh-server-include'), , { jsAttrName: 'data-bem', jsAttrScheme: 'json' } ],
+            require('enb/techs/files'),
+            require('enb/techs/deps-old'),
             require("enb/techs/bemdecl-from-bemjson"),
-            require("enb/techs/deps-old"),
-            require("enb/techs/files"),
-            require("enb/techs/css"),
-            [ require("enb/techs/browser-js"), { target: '?.js'} ],
-            [ require("bh/techs/bh-server"), { jsAttrName: 'data-bem', jsAttrScheme: 'json' } ],
-            require("enb/techs/html-from-bemjson")
+            [ require("enb/techs/file-provider"), { target: "?.bemjson.js" } ],
+            [ require("enb/techs/levels"), {levels: getLevels()} ]
+
+        //     require("enb/techs/deps-old"),
+        //     require("enb/techs/css"),
+        //     [ require("enb/techs/browser-js"), { target: '?.js'} ],
+        //     [ require("bh/techs/bh-server"), { jsAttrName: 'data-bem', jsAttrScheme: 'json' } ],
+        //     require("enb/techs/html-from-bemjson")
         ]);
-        nodeConfig.addTargets(["?.html", "?.css", "?.js"]);
+
+        // nodeConfig.addTargets(["?.html", "?.css", "?.js"]);
+        nodeConfig.addTargets(["?.bh.js"]);
 
         function getLevels() {
             return [
@@ -29,31 +34,4 @@ module.exports = function(config) {
             ].map(function(l) { return config.resolvePath(l); });
         }
     });
-}
-
-// module.exports = function (config) {
-//     config.node('pages/index', function (nodeConfig) {
-//         nodeConfig.addTechs([
-//             require('enb/techs/bemdecl-from-bemjson')
-//             [ require('enb/techs/levels'), { levels: getLevels(config) } ],
-//             [ require('enb/techs/file-provider'), { target: '?.bemdecl.js' } ],
-//             require('enb/techs/deps-old'),
-//             require('enb/techs/files'),
-//             require('enb/techs/js'),
-//             require('enb/techs/css'),
-//             [ require('enb/techs/file-copy'), { sourceTarget: '?.js', destTarget: '_?.js' } ],
-//             [ require('enb/techs/file-copy'), { sourceTarget: '?.css', destTarget: '_?.css' } ]
-//         ]);
-
-//         nodeConfig.addTargets(['_?.js', '_?.css']);
-//     });
-// };
-
-// function getLevels(config) {
-//     return [
-//       { path: 'bem-core/common.blocks', check: false },
-//       { path: 'bem-core/desktop.blocks', check: false },
-//       // 'common.blocks',
-//       'desktop.blocks'
-//     ].map(function(levelPath) { return config.resolvePath(levelPath); });
-// }
+};
